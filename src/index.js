c095ed0c65533aa3c094139e89ca003a89cda303
Sascha <sascha.picard@googlemail.com>
2016-05-26 20:58:11 +0200

    found the problem: blob type not supported

--- src/index.js
+++ src/index.js
@@ -1,18 +1,50 @@
 var UTILS = require('./utils');
+require('leaflet');
+require('leaflet_css');
 
+
+L.TileLayer.MBTiles = require('../lib/TileLayer.MBTiles');
 
 $(document).ready(function () {
     document.addEventListener("deviceready", onDeviceReady, true); // this gets called once phonegap/cordova api is running
 });
 function onDeviceReady() {
 
-    window.sqlitePlugin.echoTest(successCallback, errorCallback);
-    function successCallback() {
-        UTILS.logIt('successCallback')
-    }
-    function errorCallback() {
-        UTILS.logIt('errorCallback')
-    }
-   // var db = window.sqlitePlugin.openDatabase({name: "Tiles.db", location: 'default', createFromLocation: 1});
+    UTILS.logIt('accessing db');
+    var db = window.sqlitePlugin.openDatabase({name: "tiles.db",
+        location: 'default', // in www folder
+        createFromLocation: 1,
+        androidLockWorkaround: 1}, // https://github.com/trevorpowell/cordova-sqlite-ext#workaround-for-android-db-locking-issue
+        function () {
+            UTILS.logIt('DB opened')
+        },
+        function () {
+            UTILS.logIt('ERROR could not open db: ' + JSON.stringify(err));
+        }
+
+    );
+
+
+    db.transaction(function(tx) {
+        tx.executeSql("SELECT * from  tiles limit 10", [], function (tx, res) {
+            UTILS.logIt("stringify: " + JSON.stringify(res.rows.item(0)));
+        });
+    });
+    
+
+    var mbTiles = new L.TileLayer.MBTiles(
+        '', {
+            maxZoom: 15, // TODO: once all this works, min/max-zoom should be determined dynamically
+            tms : true
+        }, db);
+
+    var map = L.map('map', {
+        center: [8.65, 39.54],
+        zoom: 7,
+        layers: [mbTiles]
+    });
+
+    
+    
 
 }
